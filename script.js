d8272c0f1c2785b2d3fd11bcd75091a606579e62
zezokam <125836314+zezokam@users.noreply.github.com>
2023-09-02 20:15:29 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,58 @@
+document.getElementById("submit").addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent form submission
+
+    const subject = document.getElementById("subject").value;
+    const category = document.getElementById("category").value;
+    const department = document.getElementById("department").value;
+
+    const databaseId = "f62c207a744c4afc91e29eb93eb3b55f"; // Your database ID
+    const apiKey = "secret_sb4Xhq0OxjZUm0mJxc1Q5xY9NhxFYkqYuvnmpkgdB4O"; // Your API token
+
+    fetch(`https://api.notion.com/v1/pages/${databaseId}`, {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${apiKey}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            parent: { database_id: databaseId },
+            properties: {
+                "الموضوع": {
+                    title: [
+                        {
+                            text: {
+                                content: subject,
+                            },
+                        },
+                    ],
+                },
+                "التصنيف": {
+                    rich_text: [
+                        {
+                            text: {
+                                content: category,
+                            },
+                        },
+                    ],
+                },
+                "الدائرة": {
+                    select: {
+                        name: department,
+                    },
+                },
+            },
+        }),
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        console.log("Entry added to Notion:", data);
+        alert("تمت إضافة البيانات إلى قاعدة البيانات!");
+        document.getElementById("subject").value = "";
+        document.getElementById("category").value = "";
+        document.getElementById("department").value = "الإدارية"; // Reset the select field
+    })
+    .catch((error) => {
+        console.error("Error adding entry to Notion:", error);
+        alert("حدث خطأ أثناء إضافة البيانات إلى قاعدة البيانات.");
+    });
+});
