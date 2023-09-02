0eb81a2acea741e0b76685f57a45652b8bc2924f
zezokam <125836314+zezokam@users.noreply.github.com>
2023-09-02 20:22:11 +0400

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ document.getElementById("submit").addEventListener("click", function (event) {
     const subject = document.getElementById("subject").value;
     const category = document.getElementById("category").value;
     const department = document.getElementById("department").value;
+    const content = document.getElementById("content").value; // Get the content value
 
     const databaseId = "f62c207a744c4afc91e29eb93eb3b55f"; // Your database ID
     const apiKey = "secret_sb4Xhq0OxjZUm0mJxc1Q5xY9NhxFYkqYuvnmpkgdB4O"; // Your API token
@@ -40,6 +41,15 @@ document.getElementById("submit").addEventListener("click", function (event) {
                         name: department,
                     },
                 },
+                "المحتوى": {
+                    rich_text: [
+                        {
+                            text: {
+                                content: content,
+                            },
+                        },
+                    ],
+                },
             },
         }),
     })
@@ -49,7 +59,8 @@ document.getElementById("submit").addEventListener("click", function (event) {
         alert("تمت إضافة البيانات إلى قاعدة البيانات!");
         document.getElementById("subject").value = "";
         document.getElementById("category").value = "";
-        document.getElementById("department").value = "الإدارية"; // Reset the select field
+        document.getElementById("department").value = "الإدارية";
+        document.getElementById("content").value = ""; // Reset the content field
     })
     .catch((error) => {
         console.error("Error adding entry to Notion:", error);
